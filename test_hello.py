df0be6ca6dcc7d3231ae34121eb0794107932377
am892421 <ashiq.mohamed@broadcom.com>
2021-10-06 14:00:29 +0530

    Revert "test commit from Ashiq for Jenkins"
    This reverts commit 40520689

--- test_hello.py
+++ test_hello.py
@@ -1,9 +1,7 @@
 from application import app
 
-
 def test_hello():
     response = app.test_client().get('/')
-    print('Response from test: {}'.format(response))
 
     assert response.status_code == 200
     assert response.data == b'This is Devops Assignment - Bits Pilani!'
