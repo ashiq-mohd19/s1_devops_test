4052068966c8e61e380e170cd20dd2db4e222244
am892421 <ashiq.mohamed@broadcom.com>
2021-10-06 13:52:09 +0530

    test commit from Ashiq for Jenkins

--- test_hello.py
+++ test_hello.py
@@ -1,7 +1,9 @@
 from application import app
 
+
 def test_hello():
     response = app.test_client().get('/')
+    print('Response from test: {}'.format(response))
 
     assert response.status_code == 200
     assert response.data == b'This is Devops Assignment - Bits Pilani!'
